70966b9a980f01b900182b117827303f9434d24e
Piyush patil <098piyushpatil@gmail.com>
2025-02-27 19:16:46 +0530

    feat: add post retrieval and deletion functionality; implement error handling and user authorization

--- Backend/controller/post.controller.js
+++ Backend/controller/post.controller.js
@@ -58,9 +58,53 @@ export const updatePost = async(req,res) => {
 
 
 export const deletePost = async(req,res) => {
+    try {
+        const {paramId} = req.params
+        const user = req.user?._id
+    
+        if(!paramId) return res.status(400).json({success:false , message:"Post not found"}) 
+        if(!user) return res.status(400).json({success: false , message:"Log in to delete the post"})
+    
+        if(paramId !== user) return res.status(400).json({success:false , message:"You cannot delete another author post"})
+    
+        const deletePost = await Post.findByIdAndDelete(paramId)
+        if(!deletePost) return res.status(400).json({success: false , message:"The post to be delete cannot be found"})
+    
+        return res
+        .status(200)
+        .json({success:true , message:"Post deleted successfully"})
 
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({success:false , message:"Internal Server Error"})
+    }
 }
 
 export const getAllPosts = async(req,res) => {
-    
+    try {
+        const posts = await Post.find().sort({createdAt: -1})
+        return res.status(200).json({success: true , message:"All post fetch successfully" , posts})
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({success:false, message:"Internal server error"})
+    }
+}
+
+export const getPostById = async(req, res) => {
+    try {
+      const {postId} = req.params
+      
+      const post = await Post.findById(postId)
+      if(!post){
+        return res.status(400).json({success: false , message: "Post Not Found"})
+      }
+
+      return res
+      .status(200)
+      .json({success: true , post})
+      
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({success: false , message: "Internal Server Error"})
+    }
 }
--- Backend/routes/post.routes.js
+++ Backend/routes/post.routes.js
@@ -1,11 +1,12 @@
 import e from "express"
-import { createPost, deletePost, getAllPosts, updatePost } from "../controller/post.controller.js";
+import { createPost, deletePost, getAllPosts, getPostById, updatePost } from "../controller/post.controller.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 const router = e.Router()
 
 router.post("/createPost" ,verifyJWT, createPost)
 router.get("/getAllPost", verifyJWT , getAllPosts)
-router.put("/updatePost", verifyJWT , updatePost)
-router.delete("/delete" , verifyJWT , deletePost)
+router.get("/:id", verifyJWT, getPostById)
+router.put("/:id", verifyJWT , updatePost)
+router.delete("/:id" , verifyJWT , deletePost)
 
 export default router;
